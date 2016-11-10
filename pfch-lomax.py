f314641d8e8210f8a3401f64d469fd8ee21d0430
kolbeeee <kolberesnick@gmail.com>
2016-11-10 14:22:26 -0500

    Updated Scrape

--- pfch-lomax.py
+++ pfch-lomax.py
@@ -1,14 +1,80 @@
 from bs4 import BeautifulSoup
 import requests
+import json
 
-url = "http://research.culturalequity.org/get-audio-ix.do?ix=session&id=11&idType=collectionId&sortBy=abc"
+recordings = []
 
-bessie_jones = requests.get(url)
-page_url = bessie_jones.html
+url = "http://research.culturalequity.org/get-audio-detailed-recording.do?recordingId=23457"
 
-jones_scrape = BeautifulSoup(page_html, "html.parser")
+alan_lomax = requests.get(url)
+page_url = alan_lomax.text
 
-dates = soup.findall("td": "class", attrs={"a": "href"})
+lomax_scrape = BeautifulSoup(page_url, "html.parser")
 
-	for recordings in dates:
-		print(dates)
+trs = lomax_scrape.find_all("tr")
+
+for recording_info in trs:
+	print(recording_info)
+
+	data = recording_info.find_all("td", attrs={"class": "NormalPad5"})
+	for td in data:
+		print(td.text)
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#url = "http://research.culturalequity.org/get-audio-ix.do?ix=session&id=11&idType=collectionId&sortBy=abc"
+
+#bessie_jones = requests.get(url)
+#page_url = bessie_jones.text
+
+#jones_scrape = BeautifulSoup(page_url, "html.parser")
+
+#trs = jones_scrape.find_all("tr", attrs={"class":"odd"})
+
+#for recordings in trs:
+	#print(recordings)
+
+#	tds = recordings.find_all("td")
+#	name = tds[0].text
+
+
+
+	#artist = findall("a"})
+	#date = findall("td", attrs={"class":"displayTagTDCol"})
+	#location = findall("")
